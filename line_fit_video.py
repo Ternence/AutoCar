d1f177a7275b044cc30b0d0c93f7d24ff09d0628
zhangtengyuan <zhangtengyuan23@163.com>
2018-05-15 01:38:57 +0000

    [feature-py27] clean try catch cv and init control

--- line_fit_video.py
+++ line_fit_video.py
@@ -12,19 +12,19 @@ import sys
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 
-from car import*
-
 from combined_thresh import combined_thresh
 from perspective_transform import perspective_transform
 from Line import Line
 from line_fit import line_fit, tune_fit, final_viz, calc_curve, calc_vehicle_offset
 from moviepy.editor import VideoFileClip
 
+ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=1)
+
 # Global variables (just to make the moviepy video annotation work)
-with open('calibrate_camera.p', 'rb') as f:
-    save_dict = pickle.load(f)
-mtx = save_dict['mtx']
-dist = save_dict['dist']
+# with open('calibrate_camera.p', 'rb') as f:
+#     save_dict = pickle.load(f)
+# mtx = save_dict['mtx']
+# dist = save_dict['dist']
 window_size = 5  # how many frames for line smoothing
 left_line = Line(n=window_size)
 right_line = Line(n=window_size)
@@ -35,15 +35,19 @@ left_lane_inds, right_lane_inds = None, None  # for calculating curvature
 
 # MoviePy video annotation will call this function
 def annotate_image(img_in):
+
     """
     Annotate the input image with lane line markings
     Returns annotated image
     """
     global mtx, dist, left_line, right_line, detected
     global left_curve, right_curve, left_lane_inds, right_lane_inds
-
+    #cv2.imshow("Frame2", img_in)
+    cv2.imwrite('frametest1.jpg', img_in)
     # Undistort, threshold, perspective transform
     undist = img_in
+    # plt.imshow(img_in, cmap='gray', vmin=0, vmax=1)
+    # plt.show()
     # undist = cv2.undistort(img_in, mtx, dist, None, mtx)
     img, abs_bin, mag_bin, dir_bin, hls_bin = combined_thresh(undist)
     binary_warped, binary_unwarped, m, m_inv = perspective_transform(img)
@@ -100,37 +104,35 @@ def annotate_image(img_in):
 
     vehicle_offset = calc_vehicle_offset(undist, left_fit, right_fit)
 
-    distance = get_distance()
-    set_speed(TURN_LEFT,)
-    set_turn()
+    print(vehicle_offset)
 
     # Perform final visualization on top of original undistorted image
     result = final_viz(undist, left_fit, right_fit, m_inv, left_curve, right_curve, vehicle_offset)
 
     return result
 
-#
-# def commonControl(vehicle_offset):
-#     ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=1)
-#     start = time.clock()
-#     end = time.clock()
-#     print('duty cycle is %s' % (end - start))
-#     start = time.clock()
-#     response = ser.readline()
-#     s_r = response
-#     temp = s_r.split(":")
-#     temp = temp[1].split(",")
-#     power = string.atof(temp[0])
-#     left_speed = string.atof(temp[1])
-#     right_speed = string.atof(temp[2])
-#     sonar = string.atof(temp[3].strip())
-#     print('msg is %f %f %f %f' % (power, left_speed, right_speed, sonar))
-#     # global power, radius
-#     #
-#     # radius = vehicle_offset * 0.5
-#     ser.write('RASPI: %s, %s\n' % ('50.0', vehicle_offset))
-#     sys.argv[1], sys.argv[2]
-#     time.sleep(0.03)
+
+def commonControl(vehicle_offset):
+    start = time.clock()
+    end = time.clock()
+    print('duty cycle is %s' % (end - start))
+    start = time.clock()
+    response = ser.readline()
+    s_r = response
+    temp = s_r.split(":")
+    temp = temp[1].split(",")
+    power = string.atof(temp[0])
+    left_speed = string.atof(temp[1])
+    right_speed = string.atof(temp[2])
+    sonar = string.atof(temp[3].strip())
+    print('msg is %f %f %f %f' % (power, left_speed, right_speed, sonar))
+    # global power, radius
+    #
+    # radius = vehicle_offset * 0.5
+    # ser.write('RASPI: %s, %s\n' % ('50.0', vehicle_offset))
+    ser.write('RASPI: 10.0, 10.0\n')
+    sys.argv[1], sys.argv[2]
+    time.sleep(0.03)
 
 
 def annotate_video(input_file, output_file):
@@ -144,20 +146,25 @@ if __name__ == '__main__':
     # Annotate the video
     # annotate_video('project_video1.mp4', 'out1.mp4')
     camera = PiCamera()
-    camera.resolution = (640, 480)
+    camera.resolution = (1280, 720)
     camera.framerate = 32
-    rawCapture = PiRGBArray(camera, size=(640, 480))
+    rawCapture = PiRGBArray(camera, size=(1280, 720))
     time.sleep(0.1)
 
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         # grab the raw NumPy array representing the image, then initialize the timestamp
         # and occupied/unoccupied text
-        image = frame.array
 
+        img_in = frame.array
+        # cv2.imshow("Frame", img_in)
         # show the frame
-        cv2.imshow("Frame", image)
 
-        resultImage = annotate_image(image)
+        try:
+            annotate_image(img_in)
+        except:
+            pass
+
+        commonControl(10.0)
 
         key = cv2.waitKey(1) & 0xFF
 
